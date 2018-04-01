c35c0d28eefb1ca51636e05fc49cb461582021e6
Seth <s.zim517@gmail.com>
2018-04-01 15:41:41 -0400

    Adds image and animation to main page

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,39 @@
 const $navBtn = $('[data-hamburger-button]');
 const $navBtnBars = $('[data-hamburger-bars]');
 const $navMenu = $('[data-nav-menu]');
+const $jumperTop = $('[data-jumper-top]');
+const $jumperBottom = $('[data-jumper-bottom]');
 
 let clickHandlers = () => {
     $navBtn.click(() => {
         $navBtnBars.toggleClass('rotated');
         $navMenu.toggleClass('translate-menu');
     });
+}
 
+let topJumperLoad = () => {
+    setTimeout(() => {
+        $jumperTop.removeClass('hidden');
+        $jumperTop.addClass('jumper-text-top');
+    }, 500)
+}
 
+let bottomJumperLoad = () => {
+    setTimeout(() => {
+        $jumperBottom.removeClass('hidden');
+        $jumperBottom.addClass('jumper-text-bottom');
+    }, 3000)
+}
+
+let mainPageLoadAnimations = () => {
+    window.onload = () => {
+        topJumperLoad();
+        bottomJumperLoad();
+    }
 }
 
 const main = () => {
+    mainPageLoadAnimations();
     clickHandlers();
 }
 

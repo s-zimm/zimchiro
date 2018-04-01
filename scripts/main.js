1d3c1fdb0912315ef8c6887c1dbad1fcff82767b
Seth <s.zim517@gmail.com>
2018-04-01 14:59:05 -0400

    Creates dropdown menu for mobile. Currently only optimized for mobile.

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,18 @@
-const $navBtnInactive = $('[data-hamburger-inactive]');
-const $navBtnActive = $('[data-hamburger-active]');
+const $navBtn = $('[data-hamburger-button]');
+const $navBtnBars = $('[data-hamburger-bars]');
+const $navMenu = $('[data-nav-menu]');
+
+let clickHandlers = () => {
+    $navBtn.click(() => {
+        $navBtnBars.toggleClass('rotated');
+        $navMenu.toggleClass('translate-menu');
+    });
+
+
+}
+
+const main = () => {
+    clickHandlers();
+}
+
+main();
